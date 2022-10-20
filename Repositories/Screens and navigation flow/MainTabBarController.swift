f5a73cd117b647d8f14784242154d86bd465e451
Frederico Schnekenberg <frederico.schnekenberg.-nd@disney.com>
2022-10-20 15:07:07 -0300

    Update `MainTabBarController` with the transducer

--- Repositories/Screens and navigation flow/MainTabBarController.swift
+++ Repositories/Screens and navigation flow/MainTabBarController.swift
@@ -12,29 +12,65 @@ class MainTabBarController: UITabBarController, Networked, MainCoordinated, Logi
     weak var mainCoordinator: MainFlowCoordinator?
     weak var loginCoordinator: LoginFlowCoordinator?
     
+    private let transducer: MainTabBarControllerTransducer = MainTabBarControllerTransducer()
     private var root = FetchableValue<GitHubRoot>(url: GitHubEndpoint.apiRootURL, value: .notFetched)
+    private var error: Error?
+    
+    var isTabBarEnabled: Bool = true {
+        didSet {
+            tabBar.items?.forEach( { $0.isEnabled = isTabBarEnabled })
+        }
+    }
+    
+    var isClientAuthenticated: Bool {
+        return networkController?.isClientAuthenticated ?? false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        isTabBarEnabled = false
+        transducer.delegate = self
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        guard case .notFetched = root.value else {
-            return
-        }
-        
-        guard (networkController?.isClientAuthenticated ?? true) else {
-            loginCoordinator?.mainViewControllerRequiresAuthentication(self, isAppLaunch: true)
-            return
-        }
-        
-        networkController?.fetchValue(for: GitHubEndpoint.apiRootURL) { [weak self] (result: Result<GitHubRoot>) in
-            if let root = try? result.get(), let strongSelf = self {
-                strongSelf.root.value = .fetched(value: root)
-                strongSelf.mainCoordinator?.mainViewController(strongSelf, didLoadGitHubRoot: root)
-            }
-        }
+        transducer.fire(.load(authenticated: isClientAuthenticated))
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         loginCoordinator?.configure(viewController: segue.destination)
+    }
+}
+
+extension MainTabBarController: MainTabBarControllerTransducerDelegate {
+    func fetchRoot() {
+        networkController?.fetchValue(for: GitHubEndpoint.apiRootURL) { [weak self] (result: Result<GitHubRoot>) in
+            do {
+                let root = try result.get()
+                self?.root.value = .fetched(value: root)
+                self?.transducer.fire(.rootFetched)
+            } catch {
+                let authenticated = (error as? NetworkError) != .unauthorized
+                self?.error = error
+                self?.transducer.fire(.errorOccurred(authenticated: authenticated))
+            }
+        }
+    }
+        
+    func finishLaunch() {
+        isTabBarEnabled = true
+        root.fetchedValue.map { mainCoordinator?.mainViewController(self, didLoadGitHubRoot: $0) }
+    }
+    
+    func authenticate() {
+        loginCoordinator?.mainViewControllerRequiresAuthentication(self, isAppLaunch: error == nil)
+    }
+    
+    func showAlert() {
+        guard let error = error as? NetworkError else { return }
+        
+        mainCoordinator?.showAlert(for: error, withActionTitle: "Retry") { [weak self] in
+            self.map { $0.transducer.fire(.load(authenticated: $0.isClientAuthenticated)) }
+        }
     }
 }
